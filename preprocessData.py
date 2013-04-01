833258d20d1171d3a91e7c3cef51607a187d5d13
Julosaure <Jul@saure.org>
2013-03-31 22:43:42 -0400

    Ongoing work on preprocessing the input training csv data.

--- preprocessData.py
+++ preprocessData.py
@@ -1,33 +1,46 @@
 
-import sys, argparse, csv
+import sys, argparse, csv, string
 import lcFormat
 
 #----------------------------------------------------
 
 # training data file in csv format
 trainData="./data/LoanStats.h100.csv"
+#trainData="./data/InFunding2StatsNew (1).csv"
 
 #---------------------------------------------------
 
-def readCsv():
+def readCsv(data):
     nbLinesRead = 0
     nbLinesParsed = 0
-    records = []
-    with open(trainData, "r") as trainDataCsv:
-        trainDataReader = csv.reader(trainDataCsv, delimiter=",")
-        for row in trainDataReader:
+    rows = []
+    with open(data, "r") as dataCsv:
+        dataReader = csv.reader(dataCsv, delimiter=",")
+        for row in dataReader:
             nbLinesRead += 1
             # skip non valid csv lines likes comments or headers
-            if len(row) == lcFormat.NB_COL:
-                nbLinesParsed += 1
-                records.append(row)
-                #print "XX".join(row)
-    print "nbLinesRead="+str(nbLinesRead)
-    print "nbLinesParsed="+str(nbLinesParsed)
+            if len(row) != lcFormat.NB_COL:
+                continue
+            # skip csv column names
+            if row[0].lower() == "loan id":
+                continue
+            nbLinesParsed += 1
+            rows.append(row)
+                
+    print "nb rows read="+str(nbLinesRead)
+    print "nb rows parsed="+str(nbLinesParsed)
+    return rows
+
+def csv2Records(rows):
+    converter = lcFormat.lcRow2Rec()
+    records = map(converter.row2Rec, rows)
     return records
 
-def removeIllFormed():
-    pass
+def removeIllFormed(records):
+    # filter records without correct class labels
+    records = filter(lambda record: record.target in lcFormat.targets, records)
+    print "nb records well formed="+str(len(records))
+    return records
 
 def normalize():
     pass
@@ -36,7 +49,9 @@ def transform2Matrix():
     pass
 
 def main():
-    readCsv()
+    rows = readCsv(trainData)
+    records = csv2Records(rows)
+    records = removeIllFormed(records)
 
 if __name__ == "__main__":
     main()
